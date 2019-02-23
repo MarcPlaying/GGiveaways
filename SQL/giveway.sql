2ca6795dfedb0edf290be30f24b0329b455dc780
Marc Andre Herpers <herpersmarcandre3@gmail.com>
2019-02-23 21:45:49 +0100

    updated SQL

--- SQL/giveway.sql
+++ SQL/giveway.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 23. Feb 2019 um 21:44
+-- Server-Version: 10.1.36-MariaDB
+-- PHP-Version: 7.2.10
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -9,6 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Datenbank: `giveway`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `giveways`
+--
+
 CREATE TABLE `giveways` (
   `id` int(11) NOT NULL,
   `enabled` int(11) NOT NULL,
@@ -16,8 +35,15 @@ CREATE TABLE `giveways` (
   `Title` varchar(300) NOT NULL,
   `Description` varchar(1000) NOT NULL,
   `Giveway` text NOT NULL,
-  `Timestamp` text NOT NULL
+  `Timestamp` text NOT NULL,
+  `views` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -26,16 +52,35 @@ CREATE TABLE `users` (
   `cookie` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `giveways`
+--
 ALTER TABLE `giveways`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indizes für die Tabelle `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
 
+--
+-- AUTO_INCREMENT für Tabelle `giveways`
+--
 ALTER TABLE `giveways`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT für Tabelle `users`
+--
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
